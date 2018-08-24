e09fbdc4ee92871ab9428a0cf91df379c82b5c87
muditgarg188 <42585001+muditgarg188@users.noreply.github.com>
2018-08-25 00:10:07 +0530

    Update DivisorCount.java

--- DivisorCount.java
+++ DivisorCount.java
@@ -12,11 +12,7 @@ public class DivisorCount {
 		int count = 0;
 		for(int i = 1; i <= Math.sqrt(n); i++) {
 			if(n%i == 0) {
-				//System.out.println(i);
-				if(n % (n/i) == 0) {
-					count ++;
-					//System.out.println(n/i);
-				}
+				count ++;
 				count++;
 			}
 		}
